f632ad3e9bc1ad4a140bc2501b5921839bc826f1
angelharper <angelharper9203@gmail.com>
2023-04-20 15:19:47 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+* main -prints a given phrase
+* RETURN: 0 if success, non zero-otherwise
+*/
+int main()
+{       
+puts("\"Programming is like building a multilingual puzzle");   
+return (0);
+}
