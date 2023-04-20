1440d21ffed1849caccafab6ef421ee02ba07554
angelharper <angelharper9203@gmail.com>
2023-04-20 15:24:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
 * main -prints a given phrase
-* RETURN: 0 if success, non zero-otherwise
+* Return: Always 0 if success, non zero-otherwise
 */
-int main()
-{       
-puts("\"Programming is like building a multilingual puzzle");   
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
