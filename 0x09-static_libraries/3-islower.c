39af4dfa9b4ebfce64f606211a06e10e1caa2eca
angelharper <angelharper9203@gmail.com>
2023-05-12 15:48:43 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lower case
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @i:  is an integer
+ *
+ * Return: integer
+ */
+
+int _abs(int i)
+{
+if (i > 0)
+return (i);
+else if (i < 0)
+return (-i);
+else
+return (0);
+}
